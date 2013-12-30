11d430432ec1dd4c7fb23b2ec2710ae858e50676
ollien <njk828@gmail.com>
2013-12-30 12:29:03 -0800

    Change dots to correct binary order.

--- src/main.c
+++ src/main.c
@@ -29,7 +29,31 @@ char* itoa(int val, int base){
 	return &buf[i+1];
 	
 }
- 
+ char * toBinary(int num, int slots){
+	char * buffer =malloc (slots+1);
+	int n,c,k;
+	n=num;
+	int i=0;
+	int count=32;
+//	int oneHit=0;
+    for (c = 31; c >= 0; c--)
+     {
+       k = n >> c;
+
+       if (k & 1){
+		   if (count<=slots){
+		   buffer[i]='1';
+		   i++;
+	   		}
+	   }
+       else if (count<=slots){
+			buffer[i]='0';
+		   i++;
+	   }
+	   count--;
+     }
+	 return buffer;
+}
 int stoi(char *str)
 {
 	int res = 0;
@@ -90,6 +114,8 @@ void draw_dot_with_text(GContext *ctx,Layer * layer, GPoint center, bool filled,
 //if timeDot is true, do time, else, do date
 void draw_row(GContext *ctx,Layer *layer,unsigned int count,bool timeDot){
 	int x=15;
+	time(&now);
+	timePoint = localtime(&now);
 	char timeBuffer[3];
 	char binaryBuffer[7];
 	if (timeDot){
@@ -110,10 +136,9 @@ void draw_row(GContext *ctx,Layer *layer,unsigned int count,bool timeDot){
 		else{
 			num=stoi(timeBuffer);
 		}
-		char * itoaResult=itoa(num,2);
+		char * itoaResult=toBinary(num,count);
 		strcpy(binaryBuffer,itoaResult);
 		free(itoaResult);
-		revString(binaryBuffer);
 	}
 	else{
 		char dayBuffer[3];
@@ -124,11 +149,11 @@ void draw_row(GContext *ctx,Layer *layer,unsigned int count,bool timeDot){
 			strftime(dayBuffer,3,"%d",timePoint);	
 		}
 		int num = stoi(dayBuffer);
-		char * itoaResult=itoa(num,2);
+		char * itoaResult=toBinary(num,count);
 		strcpy(binaryBuffer,itoaResult);
 		free(itoaResult);
-		revString(binaryBuffer);
 	}
+	int q=count-1;
 	for (unsigned int i=0; i<count; i++){
 		char check;
 		if (i<strlen(binaryBuffer)){
@@ -138,16 +163,16 @@ void draw_row(GContext *ctx,Layer *layer,unsigned int count,bool timeDot){
 		 	check='0';
 		}
 		if (check=='1')
-			draw_dot_with_text(ctx,layer,GPoint(x,30*rowNum),true,i);
+			draw_dot_with_text(ctx,layer,GPoint(x,30*rowNum),true,q);
 		else
-			draw_dot_with_text(ctx,layer,GPoint(x,30*rowNum),false,i);
+			draw_dot_with_text(ctx,layer,GPoint(x,30*rowNum),false,q);
+		q--;
 		x+=23;
 	}
 	rowNum++;
 	if (rowNum==3){
 		rowNum++;
 	}
-	
 }
 void seconds_bar_callback(Layer * layer, GContext *ctx,int fourth){
 	graphics_context_set_fill_color(ctx,GColorWhite);
@@ -160,6 +185,8 @@ void add_day_of_week(Layer * layer, GContext * ctx){
 	text_layer_set_text_color(text_layer, GColorWhite);
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21 ));
 	text_layer_set_background_color(text_layer, GColorClear);
+	time(&now);
+	timePoint = localtime(&now);
 	dayBuffer="Failssday";
 	switch(timePoint->tm_wday){
 		case 0:
@@ -184,9 +211,7 @@ void add_day_of_week(Layer * layer, GContext * ctx){
 			dayBuffer="Saturday";
 			break;
 	}
-//	char * itoaResult = itoa(runCount,10);
-//	strcpy(dayBuffer,itoaResult);
-//	free(itoaResult);
+	
 			
 	text_layer_set_text(text_layer,dayBuffer);
 	layer_add_child(layer, text_layer_get_layer(text_layer));
@@ -216,11 +241,9 @@ void handle_time_change(struct tm *tick_time, TimeUnits units_changed){
 	
 	
 	//layer_remove_child_layers(display_layer);
-	layer_mark_dirty(display_layer);
 	text_layer_destroy(text_layer);
+	layer_mark_dirty(display_layer);
 	free(tick_time);
-	free(dayBuffer);
-	
 }
 
 void bluetooth_connection_callback(bool connected){
@@ -233,9 +256,6 @@ void handle_init(void) {
 	  my_window = window_create();
 	window_stack_push(my_window, true);
 	window_set_background_color(my_window, GColorBlack);
-	time(&now);
-	timePoint = localtime(&now);
-
 //	  text_layer = text_layer_create(GRect(0, 0, 144, 20));
 	window_layer = window_get_root_layer(my_window);
 	display_layer = layer_create(layer_get_bounds(window_layer));
@@ -247,11 +267,10 @@ void handle_init(void) {
 
 void handle_deinit(void) {
 	  text_layer_destroy(text_layer);
-	  window_destroy(my_window);	
-	layer_destroy(display_layer);
-	for (int i=0; i<19; i++){
-		text_layer_destroy(numLayers[i]);
-	}
+		layer_remove_child_layers(display_layer);
+	layer_mark_dirty(display_layer);
+	  window_destroy(my_window);
+	
 	tick_timer_service_unsubscribe();
 }
 
