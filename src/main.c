13061df61e20af115560f03a6305792de2444651
ollien <njk828@gmail.com>
2013-12-30 11:20:34 -0800

    Fix HUGE memory leak.

--- src/main.c
+++ src/main.c
@@ -90,8 +90,6 @@ void draw_dot_with_text(GContext *ctx,Layer * layer, GPoint center, bool filled,
 //if timeDot is true, do time, else, do date
 void draw_row(GContext *ctx,Layer *layer,unsigned int count,bool timeDot){
 	int x=15;
-	time(&now);
-	timePoint = localtime(&now);
 	char timeBuffer[3];
 	char binaryBuffer[7];
 	if (timeDot){
@@ -149,6 +147,7 @@ void draw_row(GContext *ctx,Layer *layer,unsigned int count,bool timeDot){
 	if (rowNum==3){
 		rowNum++;
 	}
+	
 }
 void seconds_bar_callback(Layer * layer, GContext *ctx,int fourth){
 	graphics_context_set_fill_color(ctx,GColorWhite);
@@ -161,8 +160,6 @@ void add_day_of_week(Layer * layer, GContext * ctx){
 	text_layer_set_text_color(text_layer, GColorWhite);
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21 ));
 	text_layer_set_background_color(text_layer, GColorClear);
-	time(&now);
-	timePoint = localtime(&now);
 	dayBuffer="Failssday";
 	switch(timePoint->tm_wday){
 		case 0:
@@ -187,9 +184,9 @@ void add_day_of_week(Layer * layer, GContext * ctx){
 			dayBuffer="Saturday";
 			break;
 	}
-	char * itoaResult = itoa(runCount,10);
-	strcpy(dayBuffer,itoaResult);
-	free(itoaResult);
+//	char * itoaResult = itoa(runCount,10);
+//	strcpy(dayBuffer,itoaResult);
+//	free(itoaResult);
 			
 	text_layer_set_text(text_layer,dayBuffer);
 	layer_add_child(layer, text_layer_get_layer(text_layer));
@@ -220,8 +217,10 @@ void handle_time_change(struct tm *tick_time, TimeUnits units_changed){
 	
 	//layer_remove_child_layers(display_layer);
 	layer_mark_dirty(display_layer);
-
+	text_layer_destroy(text_layer);
 	free(tick_time);
+	free(dayBuffer);
+	
 }
 
 void bluetooth_connection_callback(bool connected){
@@ -234,6 +233,9 @@ void handle_init(void) {
 	  my_window = window_create();
 	window_stack_push(my_window, true);
 	window_set_background_color(my_window, GColorBlack);
+	time(&now);
+	timePoint = localtime(&now);
+
 //	  text_layer = text_layer_create(GRect(0, 0, 144, 20));
 	window_layer = window_get_root_layer(my_window);
 	display_layer = layer_create(layer_get_bounds(window_layer));
@@ -245,10 +247,11 @@ void handle_init(void) {
 
 void handle_deinit(void) {
 	  text_layer_destroy(text_layer);
-		layer_remove_child_layers(display_layer);
-	layer_mark_dirty(display_layer);
-	  window_destroy(my_window);
-	
+	  window_destroy(my_window);	
+	layer_destroy(display_layer);
+	for (int i=0; i<19; i++){
+		text_layer_destroy(numLayers[i]);
+	}
 	tick_timer_service_unsubscribe();
 }
 
